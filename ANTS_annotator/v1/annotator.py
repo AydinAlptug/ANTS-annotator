7e89ce1a85bd50049643e406739bb0af04d16816
Alptug Aydin <yusufaydinceng@gmail.com>
2023-01-10 01:08:54 +0300

    add roi selection capability

--- ANTS_annotator/v1/annotator.py
+++ ANTS_annotator/v1/annotator.py
@@ -1,9 +1,10 @@
 import os
 
+import cv2
 from PyQt5.QtGui import QPixmap, QScreen
 from PyQt5.QtWidgets import QMainWindow, QRubberBand, QFileDialog
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize
 
 from window_2 import Ui_MainWindow
 
@@ -33,6 +34,8 @@ class Annotator(QMainWindow):
         self.origin = None
         self.rois = []
         self.currentRubberBand = QRubberBand(QRubberBand.Rectangle, self)
+        self.currentClickPointValid = False
+        self.currentReleasePointValid = False
         self.imageLabel = self.ui.label
         self.mode = self.ui.checkBox.isChecked()
         self.ui.textEdit_5.setDisabled(True)
@@ -68,11 +71,10 @@ class Annotator(QMainWindow):
             self.rubberBandList.clear()
             self.rois.clear()
 
-        if self.count < self.number_files - 1:
+        if self.current_image_index < self.number_files - 1:
             self.current_image_index += 1
-            self.count = self.count + 1
-
             self.showImage()
+        self.updateInfo()
 
     def prevImage(self):
         if self.current_image_index != 0:
@@ -80,12 +82,13 @@ class Annotator(QMainWindow):
             self.rubberBandList.clear()
             self.rois.clear()
 
-        if self.current_image_index - 1 >= 0:
+        if self.current_image_index > 0:
             self.current_image_index -= 1
             self.showImage()
             # self.showImage(
             #     remain='{:02d}/{}'.format(self.count + 1, self.number_files),
             #     name="%06d.jpg" % self.count)
+        self.updateInfo()
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Right:
@@ -104,7 +107,10 @@ class Annotator(QMainWindow):
         self.ui.label.setPixmap(scaledPix)
 
     def updateInfo(self):
-        pass
+        self.ui.label_2.setText("Frame: "
+                                + str(self.current_image_index + 1)
+                                + " / "
+                                + str(len(self.images)))
 
     def loadSelectedSourceFolder(self):
         self.count = 0
@@ -151,3 +157,102 @@ class Annotator(QMainWindow):
             self.screenChanged.emit(oldScreen, newScreen)
         self.showImage()
         return super().moveEvent(event)
+
+    def mousePressEvent(self, event):
+
+        q_point = QPoint(event.pos())
+
+        if event.button() == Qt.LeftButton:
+
+            h, w, c = cv2.imread(self.source_path + self.images[self.current_image_index]).shape  # current image shape
+
+            self.origin = q_point
+
+            if self.isInImage(q_point):
+                self.currentClickPointValid = True
+                newRubberBand = QRubberBand(QRubberBand.Rectangle, self)
+                newRubberBand.setGeometry(QRect(self.origin, QSize()))
+
+                if q_point.y() <= h and q_point.x() <= w:  # prevent outside boxes
+                    point = q_point
+                    if self.isInImage(point):
+                        self.rubberBandList.append(newRubberBand)
+                        newRubberBand.show()
+                        self.currentRubberBand = newRubberBand
+            else:
+                self.currentClickPointValid = False
+
+        if event.button() == Qt.RightButton:
+            point = q_point
+            for r in self.rubberBandList:
+                if self.isInRect(point, list(r.geometry().getRect())):
+                    r.hide()
+                    self.rubberBandList.remove(r)
+                    if self.rois.__contains__(list(r.geometry().getRect())):
+                        self.rois.remove(list(r.geometry().getRect()))
+                    self.origin = None
+
+    def isInRect(self, point, rect):
+        xp = point.x()
+        yp = point.y()
+
+        x1, y1, w, h = rect
+        x2, y2 = x1 + w, y1 + h
+
+        if x1 < xp and xp < x2:
+            if y1 < yp and yp < y2:
+                return True
+        return False
+
+    def isInImage(self, point):
+        xp = point.x()
+        yp = point.y()
+
+        x1 = self.ui.label.geometry().x()
+        y1 = self.ui.label.geometry().y()
+        w = self.ui.label.geometry().width()
+        h = self.ui.label.geometry().height()
+
+        x2, y2 = x1 + w, y1 + h
+
+        if x1 < xp and xp < x2:
+            if y1 < yp and yp < y2:
+                return True
+        return False
+
+    def transform(self, roi):
+        # 00 0h
+        # w0 wh
+        p = self.source_path + self.images[self.current_image_index]
+        img_raw = cv2.imread(p)
+        h, w, c = img_raw.shape
+        print("R", roi)
+        roi = [0 if x < 0 else x for x in roi]
+        if roi[0] > w or roi[1] > h:
+            return None
+        if roi[0] > w and roi[1] > h:
+            roi[0] = w
+            roi[1] = h
+        return roi
+
+    def mouseMoveEvent(self, event):
+        if not (self.origin is None or self.origin.isNull()):
+            if event.button() == Qt.RightButton:
+                return
+            point = QPoint(event.pos())
+            if self.isInImage(point) and self.currentClickPointValid:
+                self.currentRubberBand.setGeometry(QRect(self.origin, event.pos()).normalized())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            rect = list(self.currentRubberBand.geometry().getRect())
+
+            point = QPoint(event.pos())
+            if self.isInImage(point):
+                rect = self.transform(rect)
+                if rect is not None:
+                    self.currentReleasePointValid = True
+                    self.rois.append(rect)
+            else:
+                self.currentReleasePointValid = False
+            # self.rubberBand.hide()
